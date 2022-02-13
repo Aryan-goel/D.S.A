c10003953303b7d55b4eccc3498b8c44363ea5ec
Aryan goel <80237893+Aryan-goel@users.noreply.github.com>
2022-02-13 16:49:08 +0530

    using hare and tortoise algo

--- Arrays/findTheDuplicateElement.java
+++ Arrays/findTheDuplicateElement.java
@@ -1,4 +1,25 @@
-// O(NlogN) soln
+// O(N) using hare and tortoise algo(one pointer twice as faster than the other )
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        
+        do{
+            slow=nums[slow];
+            fast= nums[nums[fast]];
+        }
+        while(slow!=fast);
+            fast=nums[0];
+            while(slow!=fast){
+                slow=nums[slow];
+                fast=nums[fast];
+            }
+        
+        return slow;
+    }
+}
+
+// O(NlogN) soln(BRUTE FORCE )
 
 class Solution {
     public int findDuplicate(int[] nums) {
