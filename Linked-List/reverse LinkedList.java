821ae0c7f1305b96ad01a49e3d0cc07d155ea867
Aryan goel <80237893+Aryan-goel@users.noreply.github.com>
2022-02-19 10:22:02 +0530

    reverse the next references in the reverse order

---
+++ Linked-List/reverse LinkedList.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        ListNode prev=null;
+        while(head!=null){
+        
+        ListNode temp=head.next;
+        head.next=prev;
+        prev=head;
+        head=temp;
+        }
+        return prev;
+        
+    }
+}
