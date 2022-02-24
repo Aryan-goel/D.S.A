c596e9256b71c918dfa6df13069b459422d161db
Aryan goel <80237893+Aryan-goel@users.noreply.github.com>
2022-02-24 12:52:13 +0530

    A.K.A   LinkedList 2

---
+++ Linked-List/findTheStartingPoint.java
@@ -0,0 +1,23 @@
+//basic cycle detection using fast and slow pointer
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+    
+        ListNode fast=head;
+        ListNode slow= head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow= slow.next;
+            if(fast==slow){
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){
+            return null;
+        }
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
